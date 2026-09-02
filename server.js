960ba5ab710f8602b36587c2de33099ebd4d12a8
alzaeimmosa360alfurije-del <alzaeimmosa360alfurije@gmail.com>
2026-09-02 22:51:39 +0300

    Update menuData import to new file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const menuData = require('./data/menuData');
+const menuData = require('./data/menuData.new.js');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
